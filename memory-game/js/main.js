35d881927a288ac583f94f439e9067d27bc6b48f
Stevenson Lucas J <StevensonLucasJ@JohnDeere.com>
2020-07-28 15:10:09 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,15 @@
-let cardOne = "king";
-let cardTwo = "queen";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+const cardOne = cards[3];
+cardsInPlay.push(cardOne);
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!")
+	} else
+	alert("Sorry, try again.")
+}
